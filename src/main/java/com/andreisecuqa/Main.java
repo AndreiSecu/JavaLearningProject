fc755db2c0434139d450982b8fbd0bbda98e4b73
andsec <andrei.secu@snowsoftware.com>
2023-05-04 17:59:58 +0300

    Added the Variables and Comments Task

--- src/main/java/com/andreisecuqa/Main.java
+++ src/main/java/com/andreisecuqa/Main.java
@@ -1,4 +1,6 @@
-package com.andreisecuqa;public class Main {
+package com.andreisecuqa;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
